7eb1550d57def755110c66f05b5d13c4637cdfc9
so0126 <nada981226@gmail.com>
2025-11-27 11:40:34 +0900

    migration: update_timestamp 자동 업데이트 트리거 추가

--- migration/008_4_create_travel_table.sql
+++ migration/008_4_create_travel_table.sql
@@ -39,3 +39,17 @@ CREATE TABLE travel_places (
 		-- 원문 상세 정보 (엑셀 전체 텍스트 등)
     detail_info TEXT    
 );
+
+-- 데이터 수정될 때마다 update_timestamp 자동 업데이트 트리거 --
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_timestamp
+BEFORE UPDATE ON travel_places
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
