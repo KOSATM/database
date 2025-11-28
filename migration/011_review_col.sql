d199dc8182660376253745658a7e9886d88081a3
so0126 <nada981226@gmail.com>
2025-11-28 19:15:34 +0900

    migration: 관계도 변경

--- migration/011_review_col.sql
+++ migration/011_review_col.sql
@@ -3,4 +3,6 @@ ALTER TABLE ai_review_styles DROP COLUMN embedding;
 ALTER TABLE ai_review_styles DROP COLUMN is_trendy;
 
 ALTER TABLE ai_review_styles RENAME COLUMN description to caption;
-ALTER TABLE ai_review_posts RENAME COLUMN content to caption;
+ALTER TABLE review_posts RENAME COLUMN content to caption;
+ALTER TABLE ai_review_hashtags DROP CONSTRAINT fk_ai_review_hashtags_analysis;
+ALTER TABLE ai_review_hashtags DROP COLUMN review_analysis_id;
