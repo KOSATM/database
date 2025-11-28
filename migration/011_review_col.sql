9b32307bc5e51abed936ae1e335c24f3b10e52ef
so0126 <nada981226@gmail.com>
2025-11-28 19:12:14 +0900

    migration: caption으로 변경

--- migration/011_review_col.sql
+++ migration/011_review_col.sql
@@ -2,5 +2,5 @@ ALTER TABLE ai_review_analysis DROP COLUMN prompt_text;
 ALTER TABLE ai_review_styles DROP COLUMN embedding;
 ALTER TABLE ai_review_styles DROP COLUMN is_trendy;
 
-ALTER TABLE ai_review_styles ADD COLUMN caption TEXT;
+ALTER TABLE ai_review_styles RENAME COLUMN description to caption;
 ALTER TABLE ai_review_posts RENAME COLUMN content to caption;
